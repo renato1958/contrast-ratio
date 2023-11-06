0354b331512fc4e9ad3ecc67c39e21c1c7fb9511
renato1958 <felisi.renato@gmail.com>
2023-11-06 16:02:03 +0100

    Terminato il service worker

---
+++ sw.js
@@ -0,0 +1,60 @@
+const VERSION = "v1";
+const CACHE_NAME = `contrast-ratio-${VERSION}`;
+const APP_STATIC_RESOURCES = [
+    "/",
+    "/index.html",
+    "/css/style.css",
+    "/js/main.js",
+    "/js/functions.js",
+    "/assets/favicons/android-chrome-192x192.png",
+    "/assets/favicons/android-chrome-512x512.png",
+    "/assets/favicons/apple-touch-icon.png",
+    "/assets/favicons/favicon-16x16.png",
+    "/assets/favicons/favicon-32x32.png",
+    "/assets/favicons/favicon.ico",
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+];
+
+self.addEventListener("install", e => {
+    e.waitUntil(
+        (async () => {
+            const cache = await caches.open(CACHE_NAME);
+            cache.addAll(APP_STATIC_RESOURCES);
+        })(),
+    );
+});
+
+self.addEventListener("activate", e => {
+    e.waitUntil(
+        (async () => {
+            const names = await caches.keys();
+            await Promise.all(
+                names.map(name => {
+                    if(name !== CACHE_NAME) {
+                        return caches.delete(name);
+                    }
+                })
+            );
+            await clients.claim();
+        })(),
+    );
+});
+
+self.addEventListener("fetch", e => {
+    if(e.request.mode === "navigate") {
+        e.respondWith(caches.match("/"));
+        return;
+    }
+
+    e.respondWith(
+        (async () => {
+            const cache = await caches.open(CACHE_NAME);
+            const cachedResponse = await cache.match(e.request.url);
+            if(cachedResponse) {
+                return cachedResponse;
+            }
+
+            return new Response(null, { status: 404 });
+        })(),
+    );
+});
